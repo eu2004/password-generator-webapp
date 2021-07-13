efc3970716bbfa248c575309c6622803a16ac511
Emilian Utma <utma.emilian@gmail.com>
2021-07-13 20:02:03 +0300

    Add logger

--- src/main/java/ro/eutm/passworgenwebapp/controller/PasswordGeneratorController.java
+++ src/main/java/ro/eutm/passworgenwebapp/controller/PasswordGeneratorController.java
@@ -1,8 +1,14 @@
 package ro.eutm.passworgenwebapp.controller;
 
-import io.swagger.annotations.*;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ro.eu.passwallet.service.PasswordGenerator;
@@ -12,23 +18,28 @@ import java.util.Objects;
 @RestController
 @RequestMapping("/password-generator")
 public class PasswordGeneratorController {
+    private static final Logger logger = LoggerFactory.getLogger(PasswordGeneratorController.class);
+
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
-    @ApiOperation("generates a random password based on the given configuration. " +
-            "The minimum length of the password is 4." +
-            "The maximum is 256." +
-            "Example:" +
-            "{\n" +
-            "  \"includeLowerCase\": true,\n" +
-            "  \"includeNumbers\": true,\n" +
-            "  \"includeSymbols\": true,\n" +
-            "  \"includeUpperCase\": true,\n" +
-            "  \"length\": 16\n" +
-            "}")
-    public String generate(@RequestBody PasswordGeneratorConfig config) {
-        Objects.nonNull(config);
+    @ApiOperation("""
+            generates a random password based on the given configuration. The minimum length of the password is 4.The maximum is 256.Example:{
+              "includeLowerCase": true,
+              "includeNumbers": true,
+              "includeSymbols": true,
+              "includeUpperCase": true,
+              "length": 16
+            }""")
+    public String generate(@RequestHeader HttpHeaders headers, @RequestBody PasswordGeneratorConfig config) {
+        if (Objects.isNull(config)) {
+            logger.error(String.format("RequestBody is null %s", config));
+            headers.forEach((key, value) -> logger.info(String.format("Header '%s' = %s", key, value)));
+            return "";
+        }
 
         if (config.length > 256 || config.length < 4) {
+            logger.error(String.format("Exceeded length %s", config.length));
+            headers.forEach((key, value) -> logger.info(String.format("Header '%s' = %s", key, value)));
             return "";
         }
 
@@ -39,12 +50,15 @@ public class PasswordGeneratorController {
         passwordGenerator.setIncludeNumbers(config.includeNumbers);
         passwordGenerator.setIncludeSymbols(config.includeSymbols);
 
+        logger.debug(String.format("Generating password using %s", config));
+
         return passwordGenerator.generate();
     }
 
     @Getter
     @Setter
     @ApiModel
+    @ToString
     public static class PasswordGeneratorConfig {
         @ApiModelProperty(position = 1, required = true, value = "16", example = "16")
         private int length = 16;
